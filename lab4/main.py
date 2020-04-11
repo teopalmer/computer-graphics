5edf34d12699013ef3428725bb1ce21819d5707a
Teo <teopalmer@ya.ru>
2020-04-11 20:43:45 +0300

    Add interface

---
+++ lab4/main.py
@@ -0,0 +1,11 @@
+'''
+ Лабораторная №4
+'''
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPen, QPainter, QColor, QBrush, QImage, QPixmap, QRgba64
+from PyQt5.QtCore import Qt
+from math import cos, sin, pi, radians, copysign, fabs, trunc
+import numpy as np
+import time
+
