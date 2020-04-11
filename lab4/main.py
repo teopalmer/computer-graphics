f10dc72676690a34a3e33be48006273ad0cbf75d
Teo <teopalmer@ya.ru>
2020-04-11 21:01:45 +0300

    Set up interface

--- lab4/main.py
+++ lab4/main.py
@@ -1,5 +1,7 @@
 '''
  Лабораторная №4
+ Реализация алгоритмов построения окружности, исследование
+ и сравнение визуальных и временных характеристик алгоритмов.
 '''
 
 from PyQt5 import QtWidgets, uic
@@ -9,3 +11,49 @@ from math import cos, sin, pi, radians, copysign, fabs, trunc
 import numpy as np
 import time
 
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        uic.loadUi("window.ui", self)
+        self.scene = QtWidgets.QGraphicsScene(0, 0, 511, 511)
+        self.canvas.setScene(self.scene)
+        self.image = QImage(511, 511, QImage.Format_ARGB32_Premultiplied)
+        self.pen = QPen()
+        self.color_line = QColor(Qt.black)
+        self.color_bground = QColor(Qt.white)
+        self.clean_all.clicked.connect(lambda : clear_all(self))
+        self.bgColorButton.clicked.connect(lambda: get_color_bground(self))
+        self.lineColorButton.clicked.connect(lambda: get_color_bground(self))
+
+def get_color_bground(win):
+    color = QtWidgets.QColorDialog.getColor(initial=Qt.white, title='Цвет фона',
+                                            options=QtWidgets.QColorDialog.DontUseNativeDialog)
+    if color.isValid():
+        win.color_bground = color
+        win.image.fill(color)
+        s = QtWidgets.QGraphicsScene(0, 0, 10, 10)
+        s.setBackgroundBrush(color)
+        win.bground_color.setScene(s)
+        win.scene.setBackgroundBrush(color)
+
+
+def get_color_line(win):
+    color = QtWidgets.QColorDialog.getColor(initial=Qt.black, title='Цвет линии',
+                                            options=QtWidgets.QColorDialog.DontUseNativeDialog)
+    if color.isValid():
+        win.color_line = color
+        win.pen.setColor(color)
+        s = QtWidgets.QGraphicsScene(0, 0, 10, 10)
+        s.setBackgroundBrush(color)
+        win.line_color.setScene(s)
+
+def clear_all(win):
+    win.image.fill(Qt.color0)
+    win.scene.clear()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = Window()
+    w.show()
+    sys.exit(app.exec_())
